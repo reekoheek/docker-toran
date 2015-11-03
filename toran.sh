0012a67070ab501bf97f0748506680c5f2d800d5
Ganesha <reekoheek@gmail.com>
2015-11-03 12:57:47 +0700

    fixed config

--- toran.sh
+++ toran.sh
@@ -17,7 +17,8 @@ parameters:
 
   sed -i "s/;date\.timezone.*/date\.timezone = ${TORAN_PHP_TIMEZONE}/g" /etc/php5/cli/php.ini
 
-  ln -sf /toran-data/config.yml /toran/app/config/toran.yml
+  # ln -sf /toran-data/config.yml /toran/app/config/toran.yml
+  ln -sf /toran-data/config.yml /toran/app/toran/config.yml
 
   mkdir -p /toran/app/toran/composer
   cp -f /toran/auth.json /toran/app/toran/composer/auth.json
